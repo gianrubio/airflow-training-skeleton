94c1643e4f2f5d8c6a98f6c1e7b2c399d8848b91
Giancarlo Rubio <gianrubio@gmail.com>
2020-02-14 14:29:03 +0100

    Create LaunchOperator

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,24 @@
-import datetime
+# import datetime
 
-import pytest
-from airflow import DAG
+# import pytest
+# from airflow import DAG
 
-pytest_plugins = ["helpers_namespace"]
-
-
-@pytest.fixture
-def test_dag():
-    """Airflow DAG for testing."""
-    return DAG(
-        "test_dag",
-        default_args={"owner": "airflow", "start_date": datetime.datetime(2018, 1, 1)},
-        schedule_interval=datetime.timedelta(days=1),
-    )
+# pytest_plugins = ["helpers_namespace"]
 
 
-@pytest.helpers.register
-def run_task(task, dag):
-    """Run an Airflow task."""
-    dag.clear()
-    task.run(start_date=dag.default_args["start_date"], end_date=dag.default_args["start_date"])
+# @pytest.fixture
+# def test_dag():
+#     """Airflow DAG for testing."""
+#     return DAG(
+#         "test_dag",
+#         default_args={"owner": "airflow", "start_date": datetime.datetime(2018, 1, 1)},
+#         schedule_interval=datetime.timedelta(days=1),
+#     )
+
+
+# @pytest.helpers.register
+# def run_task(task, dag):
+#     """Run an Airflow task."""
+#     dag.clear()
+#     task.run(start_date=dag.default_args["start_date"], end_date=dag.default_args["start_date"])
 
