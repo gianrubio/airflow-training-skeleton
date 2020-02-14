060591c41878c1b685ff1af5864eae7f17aa12d8
Giancarlo Rubio <gianrubio@gmail.com>
2020-02-14 15:21:40 +0100

    Create LaunchOperator

---
+++ dags/fetch_exchange_rates.py
@@ -0,0 +1,24 @@
+from airflow import models
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+from datetime import datetime, timedelta
+from operators.http_to_gcs import HttpToGcsOperator
+
+default_args = {
+    'owner': 'AirFlow',
+    'start_date': datetime(2015, 6, 1),
+    'retry_delay': timedelta(minutes=5)
+}
+
+GCS_BUCKET = 'seth_sucks'
+
+with models.DAG('FetchExchangeRates', default_args=default_args) as dag:
+
+    upload_data = HttpToGcsOperator(
+        task_id='FetchExchangeRates',
+        endpoint='https://api.exchangeratesapi.io/history?start_at=2014-01-01&end_at=2018-01-02&symbols=EUR&base=GBP',
+        gcs_bucket=GCS_BUCKET,
+        gcs_path="bla.json",
+        dag=dag,
+    )
+
+
